2140aa501f82323e5d77ad98dc5f52bdf1d6fc02
Abhishek Nath <abhi@brooklynpacket.com>
2013-10-28 19:43:56 -0700

    pipedly initial commit

---
+++ js/functions.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	//===== User nav dropdown =====//		
+	
+	$('a.leftUserDrop').click(function () {
+		$('.leftUser').slideToggle(200);
+	});
+	$(document).bind('click', function(e) {
+		var $clicked = $(e.target);
+		if (! $clicked.parents().hasClass("leftUserDrop"))
+		$(".leftUser").slideUp(200);
+	});
+	
+	//===== Animated dropdown for the right links group on breadcrumbs line =====//
+	
+	$('.breadLinks ul li').click(function () {
+		$(this).children("ul").slideToggle(150);
+	});
+	$(document).bind('click', function(e) {
+		var $clicked = $(e.target);
+		if (! $clicked.parents().hasClass("has"))
+		$('.breadLinks ul li').children("ul").slideUp(150);
+	});
+	
+});
+
+	
